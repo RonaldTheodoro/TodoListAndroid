2ae9d0db933a9c23ffa67fd83178c478eff877c4
Ronald Theodoro Nascimento Silva <ronald.silva4@fatec.sp.gov.br>
2017-06-18 16:22:45 -0300

    add values

--- app/src/main/java/com/app/todolist/UserActivity.java
+++ app/src/main/java/com/app/todolist/UserActivity.java
@@ -4,13 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.app.todolist.dao.UserDao;
+import com.app.todolist.model.User;
 
 public class UserActivity extends AppCompatActivity {
+    private EditText editUser;
+    private EditText editLogin;
+    private EditText editPassword;
+    private UserDao userDao;
+    private User user;
+    private int idUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        userDao = new UserDao(this);
+
+        editUser = (EditText) findViewById(R.id.user_name);
+        editLogin = (EditText) findViewById(R.id.user_login);
+        editPassword = (EditText) findViewById(R.id.user_password);
     }
 
     @Override
