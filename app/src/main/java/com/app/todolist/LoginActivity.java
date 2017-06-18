40ecb07967ca2bbebe12937fc8d8ab48659d2ed8
Ronald Theodoro Nascimento Silva <ronald.silva4@fatec.sp.gov.br>
2017-06-18 01:06:38 -0300

    remove redundant variable initialization

--- app/src/main/java/com/app/todolist/LoginActivity.java
+++ app/src/main/java/com/app/todolist/LoginActivity.java
@@ -1,15 +1,20 @@
 package com.app.todolist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.app.todolist.dao.UserDao;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText editUser;
     private EditText editPassword;
+    private UserDao userDao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,26 +22,50 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         editUser = (EditText) findViewById(R.id.login_edit_user);
         editPassword = (EditText) findViewById(R.id.login_edit_password);
+        userDao = new UserDao(this);
     }
 
     public void login(View view) {
         String user = editUser.getText().toString();
         String password = editPassword.getText().toString();
-        boolean validation = true;
+        boolean validation;
 
-        if (user == null || user.equals("")) {
-            validation = false;
-            editUser.setError(getString(R.string.error_name_message));
+        validation = validateFields(
+            user, editUser, getString(R.string.error_name_message));
+
+        validation = validateFields(
+            password,
+            editPassword,
+            getString(R.string.error_password_message)
+        );
+
+        if (validation)
+            validateLogin(user, password);
+    }
+
+    private boolean validateFields(
+            String value, EditText editField, String message) {
+        if (value == null || value.equals("")) {
+            editField.setError(message);
+            return false;
         }
+        return true;
+    }
 
-        if (password == null || password.equals("")) {
-            validation = false;
-            editPassword.setError(getString(R.string.error_password_message));
-        }
+    private void validateLogin(String user, String password) {
+        if (userDao.validateLogin(user, password)) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else
+            showToastMessage("Usuario invalido");
+    }
 
-        if (validation) {
-
-        }
+    private void showToastMessage(String message) {
+        Toast.makeText(
+            getApplicationContext(),
+            message,
+            Toast.LENGTH_SHORT
+        ).show();
     }
 
     @Override
@@ -48,8 +77,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings)
-            return true;
-        return super.onOptionsItemSelected(item);
+        return (id == R.id.action_settings) || super.onOptionsItemSelected(item);
     }
 }
