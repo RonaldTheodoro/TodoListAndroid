40ec2c8b8938b60076012a2f92500e8270ab33d4
Ronald Theodoro Nascimento Silva <ronald.silva4@fatec.sp.gov.br>
2017-06-18 15:26:17 -0300

    implement auto login

--- app/src/main/java/com/app/todolist/LoginActivity.java
+++ app/src/main/java/com/app/todolist/LoginActivity.java
@@ -1,13 +1,14 @@
 package com.app.todolist;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.app.todolist.dao.UserDao;
 import com.app.todolist.util.Message;
@@ -15,15 +16,27 @@ import com.app.todolist.util.Message;
 public class LoginActivity extends AppCompatActivity {
     private EditText editUser;
     private EditText editPassword;
+    private CheckBox checkboxConnect;
     private UserDao userDao;
+    private static final String KEEP_CONNECTED = "keep_connected";
+    private static final String PREFERENCE_NAME = "LoginActivityPreferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         editUser = (EditText) findViewById(R.id.login_edit_user);
         editPassword = (EditText) findViewById(R.id.login_edit_password);
+        checkboxConnect = (CheckBox) findViewById(R.id.login_checkbox_connect);
+
         userDao = new UserDao(this);
+
+        SharedPreferences preferences = getSharedPreferences(
+            PREFERENCE_NAME, MODE_PRIVATE);
+
+        if (preferences.getBoolean(KEEP_CONNECTED, false))
+            callMainActivity();
     }
 
     public void login(View view) {
@@ -55,11 +68,27 @@ public class LoginActivity extends AppCompatActivity {
 
     private void validateLogin(String user, String password) {
         if (userDao.validateLogin(user, password)) {
-            startActivity(new Intent(this, MainActivity.class));
-            finish();
+            keepConnected();
+            callMainActivity();
         } else
             Message.showToastMessage(
                 this, getString(R.string.error_login_message));
+    }
+
+    private void callMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    private void keepConnected() {
+        SharedPreferences preferences;
+        SharedPreferences.Editor editor;
+        if (checkboxConnect.isChecked()) {
+            preferences = getSharedPreferences(PREFERENCE_NAME, MODE_PRIVATE);
+            editor = preferences.edit();
+            editor.putBoolean(KEEP_CONNECTED, true);
+            editor.apply();
+        }
     }
 
     @Override
