a81c06012a001972135a46411dafcbcff6947e03
Ronald Theodoro Nascimento Silva <ronald.silva4@fatec.sp.gov.br>
2017-06-18 00:00:20 -0300

    add onCreateOptionsMenu and onOptionsItemSelected in LoginActivity.java

--- app/src/main/java/com/app/todolist/LoginActivity.java
+++ app/src/main/java/com/app/todolist/LoginActivity.java
@@ -2,6 +2,8 @@ package com.app.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings)
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 }
