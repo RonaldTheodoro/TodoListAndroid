fe92103c5250cff2c6239550c52966d356235802
Ronald Theodoro Nascimento Silva <ronald.silva4@fatec.sp.gov.br>
2017-06-17 22:06:35 -0300

    implement onCreate and onUpgrade methods

--- app/src/main/java/com/app/todolist/dao/DatabaseHelper.java
+++ app/src/main/java/com/app/todolist/dao/DatabaseHelper.java
@@ -1,8 +1,24 @@
 package com.app.todolist.dao;
 
-/**
- * Created by ronaldtheodoro on 17/06/2017.
- */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper{
+    private static final String DATABASE = "tasks";
+    private static final int VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
