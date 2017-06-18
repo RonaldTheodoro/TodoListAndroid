f683462c662da18542c437c250ba2dbfea180164
Ronald Theodoro Nascimento Silva <ronald.silva4@fatec.sp.gov.br>
2017-06-18 20:19:28 -0300

    load user list

--- app/src/main/java/com/app/todolist/ListUsersActivity.java
+++ app/src/main/java/com/app/todolist/ListUsersActivity.java
@@ -1,18 +1,34 @@
 package com.app.todolist;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.app.todolist.adapter.UserAdapter;
+import com.app.todolist.dao.UserDao;
+import com.app.todolist.model.User;
+
+import java.util.List;
 
 public class ListUsersActivity extends AppCompatActivity {
+    private ListView listView;
+    private List<User> users;
+    private UserAdapter userAdapter;
+    private UserDao userDao;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_users);
+
+        userDao = new UserDao(this);
+        users = userDao.listUsers();
+        userAdapter = new UserAdapter(this, users);
+
+        listView = (ListView) findViewById(R.id.list_view_users);
+        listView.setAdapter(userAdapter);
     }
 
     @Override
