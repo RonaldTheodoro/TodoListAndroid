22fd946a1051bb16f281ad5d1fc6fd1af7a59f5d
Ronald Theodoro Nascimento Silva <ronald.silva4@fatec.sp.gov.br>
2017-06-20 10:59:28 -0300

    add static method addMessageOK

--- app/src/main/java/com/app/todolist/util/Message.java
+++ app/src/main/java/com/app/todolist/util/Message.java
@@ -1,6 +1,7 @@
 package com.app.todolist.util;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -8,6 +9,16 @@ public class Message {
 
     public static void showToastMessage(Activity activity, String message) {
         Toast.makeText(activity, message, Toast.LENGTH_LONG).show();
+    }
+
+    public static void addMessageOK(
+            Activity activity, String title, String message, int icon) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(activity);
+        alert.setTitle(title);
+        alert.setMessage(message);
+        alert.setNeutralButton("OK", null);
+        alert.setIcon(icon);
+        alert.show();
     }
 
     public static boolean validateFields(
