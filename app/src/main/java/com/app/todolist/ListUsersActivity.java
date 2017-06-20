a29a0ce4cd7afaf66560357f7f76a229431b2c71
Ronald Theodoro Nascimento Silva <ronald.silva4@fatec.sp.gov.br>
2017-06-20 11:21:06 -0300

    implement OnItemClickListener and OnClickListener

--- app/src/main/java/com/app/todolist/ListUsersActivity.java
+++ app/src/main/java/com/app/todolist/ListUsersActivity.java
@@ -1,10 +1,16 @@
 package com.app.todolist;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.app.todolist.adapter.UserAdapter;
@@ -13,11 +19,16 @@ import com.app.todolist.model.User;
 
 import java.util.List;
 
-public class ListUsersActivity extends AppCompatActivity {
+public class ListUsersActivity
+        extends AppCompatActivity
+        implements OnItemClickListener, OnClickListener {
     private ListView listView;
     private List<User> users;
     private UserAdapter userAdapter;
     private UserDao userDao;
+    private int itemPosition;
+    private AlertDialog alertDialog;
+    private AlertDialog alertConfirm;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,5 +55,15 @@ public class ListUsersActivity extends AppCompatActivity {
         if (id == R.id.action_register_user)
             startActivity(new Intent(this, UserActivity.class));
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
